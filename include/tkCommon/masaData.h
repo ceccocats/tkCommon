f6a7ab690a97566397936afc51641b814c3c383f
Luca Bartoli <228618@studenti.unimore.it>
2019-05-15 09:45:40 +0200

    add masa communicator struct

---
+++ include/tkCommon/masaData.h
@@ -0,0 +1,40 @@
+#include <iostream>
+
+namespace tk{ namespace common{ 
+
+struct semaphore{
+
+    int     status;
+};
+
+struct carPosition{
+
+    float   latitude;
+    float   longitude;
+    float   angle;
+};
+
+struct Obj{
+
+    float   x;      //KF
+    float   y;      //KF
+    float   yaw;    //KF
+    float   speed;  //KF
+    float   acc;    //KF
+    float   x0;
+    float   y0;
+    float   z0;
+    float   x1;
+    float   y1;
+    float   z1;
+};
+
+static const int    MAXOBJ = 50;
+
+struct ObjFusion{
+
+    int     n;
+    Obj     obj[MAXOBJ];
+};
+
+}}
