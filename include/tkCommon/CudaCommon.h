1939c74868475161bf5839330c29381ca2383646
Francesco Gatti <gattifrancesco@hotmail.it>
2019-06-18 17:14:08 +0200

    cuda common

---
+++ include/tkCommon/CudaCommon.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <cuda.h>
+#include <cuda_runtime.h>
+
+namespace tk{ namespace common {
+
+static void HandleError( cudaError_t err,
+                         const char *file,
+                         int line ) {
+    if (err != cudaSuccess) {
+        printf( "%s in %s at line %d\n", cudaGetErrorString( err ),
+                file, line );
+        exit( EXIT_FAILURE );
+    }
+}
+#define HANDLE_ERROR( err ) (HandleError( err, __FILE__, __LINE__ ))
+
+}}
