c6a83ae04112f73dd3cac932401564ec36b1c8d8
mbosi <205839@studenti.unimore.it>
2019-07-19 11:02:32 +0200

    fix to radar features

--- include/tkCommon/data/RadarData.h
+++ include/tkCommon/data/RadarData.h
@@ -18,11 +18,12 @@ namespace tk { namespace data {
         EIGEN_MAKE_ALIGNED_OPERATOR_NEW
         Eigen::Matrix<float, 4, RADAR_MAX_POINTS, Eigen::ColMajor> near_points[N_RADAR];
         Eigen::Matrix<float, 4, RADAR_MAX_POINTS, Eigen::ColMajor> far_points[N_RADAR];
+        Eigen::Matrix<float, RADAR_MAX_FEATURES, RADAR_MAX_POINTS, Eigen::ColMajor> near_features[N_RADAR];
+        Eigen::Matrix<float, RADAR_MAX_FEATURES, RADAR_MAX_POINTS, Eigen::ColMajor> far_features[N_RADAR];
         timeStamp_t             near_stamp[N_RADAR];
         timeStamp_t             far_stamp[N_RADAR];
         int                     near_n_points[N_RADAR];
         int                     far_n_points[N_RADAR];
-        float                   features[N_RADAR][RADAR_MAX_FEATURES];
         
         RadarData_t& operator=(const RadarData_t& s){
 
@@ -35,7 +36,8 @@ namespace tk { namespace data {
 
                 std::memcpy(near_points[i].data(),  s.near_points[i].data(),    near_n_points[i] * 4 * sizeof(float));
                 std::memcpy(far_points[i].data(),   s.far_points[i].data(),     far_n_points[i] * 4 * sizeof(float));
-                std::memcpy(features[i], s.features[i], RADAR_MAX_FEATURES * sizeof(float));
+                std::memcpy(near_features[i].data(), s.near_features[i].data(), near_n_points[i] * RADAR_MAX_FEATURES * sizeof(float));
+                std::memcpy(far_features[i].data(), s.far_features[i].data(), far_n_points[i] * RADAR_MAX_FEATURES * sizeof(float));
             }
         }
     };
