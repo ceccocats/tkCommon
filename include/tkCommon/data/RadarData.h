4d1d3f3aa57911e907fa07ababd0d6ff374ba12c
mbosi <205839@studenti.unimore.it>
2019-07-26 14:32:16 +0200

    added raw range to radar data

--- include/tkCommon/data/RadarData.h
+++ include/tkCommon/data/RadarData.h
@@ -18,6 +18,7 @@ namespace tk { namespace data {
         EIGEN_MAKE_ALIGNED_OPERATOR_NEW
         Eigen::Matrix<float, 4, RADAR_MAX_POINTS, Eigen::ColMajor> near_points[N_RADAR];
         Eigen::Matrix<float, 4, RADAR_MAX_POINTS, Eigen::ColMajor> far_points[N_RADAR];
+        Eigen::Matrix<float, 2, RADAR_MAX_POINTS, Eigen::ColMajor> near_ranges[N_RADAR];
         Eigen::Matrix<float, RADAR_MAX_FEATURES, RADAR_MAX_POINTS, Eigen::ColMajor> near_features[N_RADAR];
         Eigen::Matrix<float, RADAR_MAX_FEATURES, RADAR_MAX_POINTS, Eigen::ColMajor> far_features[N_RADAR];
         timeStamp_t             near_stamp[N_RADAR];
@@ -38,6 +39,7 @@ namespace tk { namespace data {
                 std::memcpy(far_points[i].data(),   s.far_points[i].data(),     far_n_points[i] * 4 * sizeof(float));
                 std::memcpy(near_features[i].data(), s.near_features[i].data(), near_n_points[i] * RADAR_MAX_FEATURES * sizeof(float));
                 std::memcpy(far_features[i].data(), s.far_features[i].data(), far_n_points[i] * RADAR_MAX_FEATURES * sizeof(float));
+                std::memcpy(near_ranges[i].data(), s.near_ranges[i].data(), near_n_points[i] * RADAR_MAX_FEATURES * sizeof(float));
             }
         }
     };
