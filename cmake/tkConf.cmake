a3f06c2d560a36a42318e457b9001fc617599bcb
Francesco Gatti <gattifrancesco@hotmail.it>
2019-09-04 16:03:50 +0200

    install path

---
+++ cmake/tkConf.cmake
@@ -0,0 +1,5 @@
+include_directories(
+    ${CMAKE_CURRENT_SOURCE_DIR}/../install/include
+    ${CMAKE_CURRENT_SOURCE_DIR}/../install/include/xspublic/
+)
+link_directories(${CMAKE_CURRENT_SOURCE_DIR}/../install/lib)
