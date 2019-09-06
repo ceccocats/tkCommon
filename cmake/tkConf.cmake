d1df23f57b76619e9eee62bc2b7eeb92af14ba98
Francesco Gatti <gattifrancesco@hotmail.it>
2019-09-06 12:19:32 +0200

    camera init with yaml object

--- cmake/tkConf.cmake
+++ cmake/tkConf.cmake
@@ -2,3 +2,7 @@ include_directories(
     ${CMAKE_INSTALL_PREFIX}/include
 )
 link_directories(${CMAKE_INSTALL_PREFIX}/lib)
+
+# suppress preprocessor warning 
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wp,-w")
+
