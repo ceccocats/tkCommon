6efffa4d29f0ff2695335400d54549e32d167ad7
Francesco Gatti <gattifrancesco@hotmail.it>
2019-09-17 15:26:23 +0200

    optional tkConf

--- cmake/tkConf.cmake
+++ cmake/tkConf.cmake
@@ -2,7 +2,3 @@ include_directories(
     ${CMAKE_INSTALL_PREFIX}/include
 )
 link_directories(${CMAKE_INSTALL_PREFIX}/lib)
-
-# suppress preprocessor warning 
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wp,-w")
-
