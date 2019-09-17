e669981e6f80655c0ab9d0888a3a7c668d3d8237
Francesco Gatti <gattifrancesco@hotmail.it>
2019-09-17 16:57:19 +0200

    TKPROJ_PATH

--- cmake/tkConf.cmake
+++ cmake/tkConf.cmake
@@ -2,3 +2,5 @@ include_directories(
     ${CMAKE_INSTALL_PREFIX}/include
 )
 link_directories(${CMAKE_INSTALL_PREFIX}/lib)
+
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DTKPROJ_PATH='\"${CMAKE_CURRENT_SOURCE_DIR}/data/\"'")
