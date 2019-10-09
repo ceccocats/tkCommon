beb1b63ef435c844fd8dcf8d02329f88fed94345
luca <228618@studenti.unimore.it>
2019-10-09 16:24:16 +0200

    update lidar data

--- include/tkCommon/data/LidarData.h
+++ include/tkCommon/data/LidarData.h
@@ -3,18 +3,42 @@
 
 namespace tk { namespace data {
 
-    #define LIDAR_MAX_POINTS 119808
+    static const int LIDAR_MAX_POINTS 200000
 
     struct LidarData_t {
-        int                                                             nPoints;
-        Eigen::Matrix<float  , 4, LIDAR_MAX_POINTS, Eigen::ColMajor>    points;
-        Eigen::Matrix<uint8_t, 1, LIDAR_MAX_POINTS, Eigen::ColMajor>    intensity;
 
+        /**
+         * @brief Points in lidar Pointcloud
+         * 
+         */
+        int                                                             nPoints;
+        
+        /**
+         * @brief Pointcloud
+         *
+         *  ( X )
+         *  ( Y )
+         *  ( Z )
+         *  ( 1 )
+         * 
+         */
+        Eigen::Matrix<float  , 4, LIDAR_MAX_POINTS, Eigen::ColMajor>    points;
+
+        /**
+         * @brief Points intensity
+         * 
+         */
+        Eigen::Matrix<float  , 1, LIDAR_MAX_POINTS, Eigen::ColMajor>    intensity;
+
+        /**
+         * @brief Overloading for struct copy
+         * 
+         */
         LidarData_t& operator=(const LidarData_t& s){
 
             nPoints = s.nPoints;
-            std::memcpy(points.data(),      s.points.data(),    nPoints * 4 * sizeof(float));
-            std::memcpy(intensity.data(),   s.intensity.data(), nPoints * sizeof(uint8_t));
+            std::memcpy(points.data(),      s.points.data(),    nPoints * 4 * sizeof(float) );
+            std::memcpy(intensity.data(),   s.intensity.data(), nPoints * sizeof(float) );
         }
     };
 
