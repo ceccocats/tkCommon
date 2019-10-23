f8706ce98d247c2ba0cc42d2d5858cdad638acd6
Francesco Gatti <gattifrancesco@hotmail.it>
2019-10-23 19:31:29 +0200

    GPSdata simplier

---
+++ include/tkCommon/data/GPSData.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <tkCommon/common.h>
+
+namespace tk { namespace data {
+
+    /**
+     *  GPS message frame definition
+     */
+    struct GPSData_t {
+        timeStamp_t stamp = 0;
+
+        double lat = 0, lon = 0, hdop =0, height = 0, age = 0;
+        int quality = 0, sats = 0;
+
+        // IMU
+        double angleX, angleY, angleZ;
+        double angleRateX, angleRateY, angleRateZ;
+        double accX, accY, accZ;
+        double sideSlip;
+
+        friend std::ostream& operator<<(std::ostream& os, const GPSData_t& m) {
+            os << "Lat/Lon: " << m.lat <<"°/" << m.lon
+               <<"°, Height: "<<m.height<<" Nsats: "<<m.sats;
+            return os;
+        }
+    };
+}
+}
