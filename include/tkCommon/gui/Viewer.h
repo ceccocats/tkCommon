cfbf272f04572372f5c6539dcfb9e366a2af260a
Fabio Bagni <228594@studenti.unimore.it>
2019-11-02 16:37:00 +0100

    Driveworks images and perception data

--- include/tkCommon/gui/Viewer.h
+++ include/tkCommon/gui/Viewer.h
@@ -71,7 +71,6 @@ namespace tk { namespace gui {
         static void tkDrawRadarData(tk::data::RadarData_t *data, bool enable_near, bool enable_far);
         static void tkDrawImage(tk::data::ImageData_t<uint8_t>& image, GLuint texture);
         static void tkSplitPanel(int count, float ratio, int &num_cols, int &num_rows, float &w, float &h, float &x, float &y);
-        void tkDrawCameraData(tk::data::CameraData_t *data, GLuint texture[], bool fullscreen = false);
 
         static void tkViewport2D(int width, int height, int x=0, int y=0);
 
