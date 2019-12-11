7abb49b2f75be23dde276f8754f3e0eb07510058
Francesco Gatti <gattifrancesco@hotmail.it>
2019-12-11 21:20:03 +0100

    loc ok

--- include/tkCommon/data/VehicleData.h
+++ include/tkCommon/data/VehicleData.h
@@ -104,6 +104,103 @@ namespace tk { namespace data {
         void init(){
             //TODO:
         }
+
+
+        friend std::ostream& operator<<(std::ostream& os, const VehicleData_t& m) {
+            os << m.header.stamp<< " speed: "<<m.speed<<" wheelangle: "<<m.wheelAngle;
+            return os;
+        }
+
+        static const int VEHICLE_FIELDS = 24;
+        const char  *fields[VEHICLE_FIELDS] = {"stamp",
+           "CAR_WHEELBASE", "CAR_DIM_X", "CAR_DIM_Y", "CAR_DIM_Z", "CAR_BACK2AXLE", "CAR_MASS", "CAR_FRONTAXLE_W", "CAR_BACKAXLE_W",
+           "CAR_WHEEL_R", "speed", "yawRate", "accelX", "accelY", "wheelAngle", "brakeMasterPressure", "engineTorque",
+           "actualGear_d", "wheelFLspeed", "wheelFRspeed", "wheelRLspeed", "wheelRRspeed", "sideSlip", "tractionGrip_d"};
+
+        /**
+         *
+         * @param name
+         * @return
+         */
+        matvar_t *toMatVar(std::string name = "gps") {
+
+            #define TK_VEHDATA_MATVAR_DOUBLE(x) var = Mat_VarCreate(#x, MAT_C_DOUBLE, MAT_T_DOUBLE, 2, dim, &x, 0); \
+                                                Mat_VarSetStructFieldByName(matstruct, #x, 0, var); //0 for first row
+
+            size_t dim[2] = { 1, 1 }; // create 1x1 struct
+            matvar_t* matstruct = Mat_VarCreateStruct(name.c_str(), 2, dim, fields, VEHICLE_FIELDS); //main struct: Data
+
+            matvar_t *var = Mat_VarCreate("stamp", MAT_C_UINT64, MAT_T_UINT64, 2, dim, &header.stamp, 0);
+            Mat_VarSetStructFieldByName(matstruct, "stamp", 0, var); //0 for first row
+            TK_VEHDATA_MATVAR_DOUBLE(CAR_WHEELBASE);
+            TK_VEHDATA_MATVAR_DOUBLE(CAR_DIM_X);
+            TK_VEHDATA_MATVAR_DOUBLE(CAR_DIM_Y);
+            TK_VEHDATA_MATVAR_DOUBLE(CAR_DIM_Z);
+            TK_VEHDATA_MATVAR_DOUBLE(CAR_BACK2AXLE);
+            TK_VEHDATA_MATVAR_DOUBLE(CAR_MASS);
+            TK_VEHDATA_MATVAR_DOUBLE(CAR_FRONTAXLE_W);
+            TK_VEHDATA_MATVAR_DOUBLE(CAR_BACKAXLE_W);
+            TK_VEHDATA_MATVAR_DOUBLE(CAR_WHEEL_R);
+            TK_VEHDATA_MATVAR_DOUBLE(speed);
+            TK_VEHDATA_MATVAR_DOUBLE(yawRate);
+            TK_VEHDATA_MATVAR_DOUBLE(accelX);
+            TK_VEHDATA_MATVAR_DOUBLE(accelY);
+            TK_VEHDATA_MATVAR_DOUBLE(wheelAngle);
+            TK_VEHDATA_MATVAR_DOUBLE(brakeMasterPressure);
+            TK_VEHDATA_MATVAR_DOUBLE(engineTorque);
+            double actualGear_d = actualGear;
+            TK_VEHDATA_MATVAR_DOUBLE(actualGear_d);
+            TK_VEHDATA_MATVAR_DOUBLE(wheelFLspeed);
+            TK_VEHDATA_MATVAR_DOUBLE(wheelFRspeed);
+            TK_VEHDATA_MATVAR_DOUBLE(wheelRRspeed);
+            TK_VEHDATA_MATVAR_DOUBLE(sideSlip);
+            double tractionGrip_d = tractionGrip;
+            TK_VEHDATA_MATVAR_DOUBLE(tractionGrip_d);
+            return matstruct;
+        }
+
+        /**
+         *
+         * @param var
+         * @return
+         */
+        bool fromMatVar(matvar_t *var) {
+
+            matvar_t *pvar = Mat_VarGetStructFieldByName(var, "stamp", 0);
+            tkASSERT(pvar->class_type == MAT_C_UINT64);
+            memcpy(&header.stamp, pvar->data, sizeof(uint64_t));
+
+            #define TK_VEHDATA_MATVAR_READ_DOUBLE(x) pvar = Mat_VarGetStructFieldByName(var, #x, 0); \
+                                                     tkASSERT(pvar != 0); \
+                                                     tkASSERT(pvar->class_type == MAT_C_DOUBLE); \
+                                                     memcpy(&x, pvar->data, sizeof(double));
+            TK_VEHDATA_MATVAR_READ_DOUBLE(CAR_WHEELBASE);
+            TK_VEHDATA_MATVAR_READ_DOUBLE(CAR_DIM_X);
+            TK_VEHDATA_MATVAR_READ_DOUBLE(CAR_DIM_Y);
+            TK_VEHDATA_MATVAR_READ_DOUBLE(CAR_DIM_Z);
+            TK_VEHDATA_MATVAR_READ_DOUBLE(CAR_BACK2AXLE);
+            TK_VEHDATA_MATVAR_READ_DOUBLE(CAR_MASS);
+            TK_VEHDATA_MATVAR_READ_DOUBLE(CAR_FRONTAXLE_W);
+            TK_VEHDATA_MATVAR_READ_DOUBLE(CAR_BACKAXLE_W);
+            TK_VEHDATA_MATVAR_READ_DOUBLE(CAR_WHEEL_R);
+            TK_VEHDATA_MATVAR_READ_DOUBLE(speed);
+            TK_VEHDATA_MATVAR_READ_DOUBLE(yawRate);
+            TK_VEHDATA_MATVAR_READ_DOUBLE(accelX);
+            TK_VEHDATA_MATVAR_READ_DOUBLE(accelY);
+            TK_VEHDATA_MATVAR_READ_DOUBLE(wheelAngle);
+            TK_VEHDATA_MATVAR_READ_DOUBLE(brakeMasterPressure);
+            TK_VEHDATA_MATVAR_READ_DOUBLE(engineTorque);
+            double actualGear_d;
+            TK_VEHDATA_MATVAR_READ_DOUBLE(actualGear_d);
+            actualGear = actualGear_d;
+            TK_VEHDATA_MATVAR_READ_DOUBLE(wheelFLspeed);
+            TK_VEHDATA_MATVAR_READ_DOUBLE(wheelFRspeed);
+            TK_VEHDATA_MATVAR_READ_DOUBLE(wheelRRspeed);
+            TK_VEHDATA_MATVAR_READ_DOUBLE(sideSlip);
+            double tractionGrip_d;
+            TK_VEHDATA_MATVAR_READ_DOUBLE(tractionGrip_d);
+            tractionGrip = tractionGrip_d;
+        }
     };
 }
 }
