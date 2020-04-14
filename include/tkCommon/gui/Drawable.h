572e72dce2c44840a481ecaa49d6d5fc49881151
Fabio Bagni <228594@studenti.unimore.it>
2020-04-14 17:09:24 +0200

    DrawBuffer

--- include/tkCommon/gui/Drawable.h
+++ include/tkCommon/gui/Drawable.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <vector>
+
 namespace tk{namespace gui{
 
 	class Drawable{
@@ -9,6 +11,24 @@ namespace tk{namespace gui{
 		virtual void draw() {}
 
 		virtual void draw2D(int width, int height, float xLim, float yLim) {}
+
+	};
+
+	class DrawBuffer : public Drawable{
+
+		std::vector<Drawable *> buffer;
+
+		virtual void draw() {
+			for(int i = 0; i < buffer.size(); i++){
+				buffer[i]->draw();
+			}
+		}
+
+		virtual void draw2D(int width, int height, float xLim, float yLim) {
+			for(int i = 0; i < buffer.size(); i++){
+				buffer[i]->draw2D(width, height, xLim, yLim);
+			}
+		}
 
 	};
 
