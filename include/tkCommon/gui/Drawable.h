d712b811043f58bee739b36d50b21e19e1c6bf37
Fabio Bagni <228594@studenti.unimore.it>
2020-04-16 15:46:44 +0200

    Fix DrawBuffer

--- include/tkCommon/gui/Drawable.h
+++ include/tkCommon/gui/Drawable.h
@@ -14,22 +14,35 @@ namespace tk{namespace gui{
 
 	};
 
+	template<typename T, typename = std::enable_if<std::is_base_of<Drawable, T>::value>>
 	class DrawBuffer : public Drawable{
 
 	public:
 
-		std::vector<Drawable *> buffer;
+		std::vector<T> buffer;
 
 		virtual void draw() {
 			for(int i = 0; i < buffer.size(); i++){
-				buffer[i]->draw();
+				buffer[i].draw();
 			}
 		}
 
 		virtual void draw2D(int width, int height, float xLim, float yLim) {
 			for(int i = 0; i < buffer.size(); i++){
-				buffer[i]->draw2D(width, height, xLim, yLim);
+				buffer[i].draw2D(width, height, xLim, yLim);
 			}
+		}
+
+		void push_back(T &obj){
+			buffer.push_back(obj);
+		}
+
+		T& pop_back(){
+			return buffer.pop_back();
+		}
+
+		void clear(){
+			buffer.clear();
 		}
 
 	};
