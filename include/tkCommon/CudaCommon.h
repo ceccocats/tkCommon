fcc1af951935f7884d310039c3db5f79caf11064
Francesco Gatti <gattifrancesco@hotmail.it>
2020-04-19 22:46:20 +0200

    cuda knn refined

--- include/tkCommon/CudaCommon.h
+++ include/tkCommon/CudaCommon.h
@@ -6,7 +6,7 @@ static void HandleError( cudaError_t err,
                          const char *file,
                          int line ) {
     if (err != cudaSuccess) {
-        printf( "%s in %s at line %d\n", cudaGetErrorString( err ),
+        printf( "%s in %s:%d\n", cudaGetErrorString( err ),
                 file, line );
         exit( EXIT_FAILURE );
     }
