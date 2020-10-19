64c103af152a087954d9477a06a959803c9a1833
lucabart97 <228618@studenti.unimore.it>
2020-10-19 20:22:18 +0200

    fix eigen

--- include/tkCommon/communication/can/VehicleCanParser.h
+++ include/tkCommon/communication/can/VehicleCanParser.h
@@ -53,7 +53,7 @@ namespace tk { namespace communication {
         }
 
         //write data
-        veh->odom.tf = tk::common::odom2tf(veh->x, veh->y, -veh->carDirection +M_PI/2).matrix();
+        veh->odom.tf.copyFrom(tk::common::odom2tf(veh->x, veh->y, -veh->carDirection +M_PI/2).matrix().data(), 4,4);
         veh->odom.header.stamp = t;
     }
 
