ce05f75538072a255d1d9e985c6354a45e2d3107
lucabart97 <228618@studenti.unimore.it>
2020-10-22 11:27:52 +0200

    vec and math class ok

--- include/tkCommon/math/Vec.h
+++ include/tkCommon/math/Vec.h
@@ -13,17 +13,46 @@
 
 namespace tk { namespace math {
 
-template<class T, int N>
-class Vec : public tk::math::MatStatic<T,1,N>{
+template<class T>
+class Vec : public tk::math::Mat<T>{
 public:
-    Vec() : MatStatic<T,1,N>(){
+    Vec() : Mat<T>(){
+
+    }
+
+    ~Vec(){
+
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Vec<T>& s) {
+        os<<"vec( "<<s.cols()<<" )";
+        return os;
+    }
+
+    __host__ void 
+    resize(int r, int c) {
+
+        tkASSERT(r == 1, "You can't create a 2D vector");
+        Mat<T>::resize(1,c);
+    }
+
+    __host__ void 
+    resize(int n) {
+        Mat<T>::resize(1,n);
+    }
+};
+
+template<class T, int N>
+class VecStatic : public tk::math::MatStatic<T,1,N>{
+public:
+    VecStatic() : MatStatic<T,1,N>(){
         for(int i = 0; i < N; i++){
             this->data_h[i] = 0;
         }
     }
-    ~Vec() {}
+    ~VecStatic() {}
     
-    friend std::ostream& operator<<(std::ostream& os, const Vec<T,N>& s) {
+    friend std::ostream& operator<<(std::ostream& os, const VecStatic<T,N>& s) {
         if(N > 4){
             os<<"vec("<<N<<")";
         }else{
@@ -40,10 +69,10 @@ public:
 };
 
 template<class T>
-class Vec2 : public Vec<T,2> {
+class Vec2 : public VecStatic<T,2> {
 public:
 
-    Vec2() : Vec<T,2>(){
+    Vec2() : VecStatic<T,2>(){
     }
 
     Vec2(T x, T y) {
@@ -59,10 +88,10 @@ public:
 };
 
 template<class T>
-class Vec3 : public Vec<T,3> {
+class Vec3 : public VecStatic<T,3> {
 public:
 
-    Vec3() : Vec<T,3>(){
+    Vec3() : VecStatic<T,3>(){
     }
 
     Vec3(T x, T y, T z) {
@@ -77,6 +106,29 @@ public:
     T& x(){return this->data_h[0];}
     T& y(){return this->data_h[1];}
     T& z(){return this->data_h[2];}
+};
+
+template<class T>
+class Vec4 : public VecStatic<T,4> {
+public:
+
+    Vec4() : VecStatic<T,4>(){
+    }
+
+    Vec4(T x, T y, T z, T w) {
+        this->x() = x;
+        this->y() = y;
+        this->z() = z;
+        this->w() = w;
+    }
+
+    ~Vec4() {
+    }
+
+    T& x(){return this->data_h[0];}
+    T& y(){return this->data_h[1];}
+    T& z(){return this->data_h[2];}
+    T& w(){return this->data_h[3];}
 };
 
 }}
