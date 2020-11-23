b45896932cedbb20cd7708ec76c10e76f5c76dd3
Fabio Bagni <228594@studenti.unimore.it>
2020-11-23 13:25:49 +0100

    Augmented reality first commit

--- include/tkCommon/data/ImageData.h
+++ include/tkCommon/data/ImageData.h
@@ -5,6 +5,61 @@
 namespace tk{ namespace data{
 
     class ImageData : public ImageData_gen{
+        public:
+
+        void init(){
+
+            lock();
+        	ImageData_gen::init();
+            unlockWrite();
+
+        }
+        
+        void init(int w, int h, int ch){
+
+            lock();
+        	ImageData_gen::init();
+            width = w;
+            height = h;
+            channels = ch;
+            data = new uint8_t[width*height*channels];
+            header.name = sensorName::CAMDATA;
+            unlockWrite();
+
+        }
+
+        ImageData& operator=(ImageData& s){
+ 
+            if(s.width != width || s.height != height || s.channels != channels){
+                release();
+                init(s.width, s.height, s.channels);
+            }
+
+            lock();
+            s.lock();
+            memcpy(data, s.data, width * height * channels);
+            s.unlockRead();
+            unlockWrite();
+            return *this;
+        }
+
+        bool empty() {return channels == 0 || width == 0 || height == 0 || data == nullptr; }
+
+        void release(){
+            if(empty())
+                return;
+
+            lock();
+            uint8_t* tmp = data;
+            data = nullptr;
+            width = 0;
+            height = 0;
+            channels = 0;
+            delete [] tmp;
+            unlockWrite();
+        }
+
+
 
     };
 }}
