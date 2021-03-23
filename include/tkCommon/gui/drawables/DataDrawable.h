427df249324df3aaa012a3d409d83cb85eaf0ac8
Luca Bartoli <228618@studenti.unimore.it>
2021-03-23 13:20:23 +0100

    fix3

--- include/tkCommon/gui/drawables/DataDrawable.h
+++ include/tkCommon/gui/drawables/DataDrawable.h
@@ -21,7 +21,7 @@ class DataDrawable : public Drawable {
         virtual void updateData(tk::gui::Viewer *viewer) = 0; 
 
         tk::data::SensorData* data = nullptr;
-        uint32_t counter = sizeof(uint32_t);
+        uint32_t counter = 9999;
 
         std::stringstream print;
 
