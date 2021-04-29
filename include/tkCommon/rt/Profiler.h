c0e66296fe05429ea05254c1b164f60fbc9b6a00
Francesco Gatti <gattifrancesco@hotmail.it>
2021-04-29 17:13:55 +0200

    ifdef tkPROF

--- include/tkCommon/rt/Profiler.h
+++ include/tkCommon/rt/Profiler.h
@@ -100,7 +100,12 @@ public:
 
 }}
 
-#ifndef tkPROF_disable
+#ifdef tkPROF_disable
+    #define tkPROF_tic(name) ;
+    #define tkPROF_toc(name) ;
+    #define tkPROF_print     ;
+
+#else
     #define tkPROF_tic(name) tk::rt::Profiler::getInstance()->tic(#name);
     #define tkPROF_toc(name) tk::rt::Profiler::getInstance()->toc(#name);
     #define tkPROF_print     tk::rt::Profiler::getInstance()->print();
