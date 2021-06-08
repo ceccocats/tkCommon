e5d977de61d76a9312ed8efac16ca83376911139
Fabio Bagni <228594@studenti.unimore.it>
2021-06-08 11:46:22 +0200

    Camera streamers as subdirectories, CameraStream.h in tkCommon

---
+++ include/tkCommon/sensor/CameraStream.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <tkCommon/data/ImageData.h>
+#include <thread>
+
+#define MAX_STREAM_COUNT 32
+
+namespace tk{namespace sensors{
+
+	class CameraContext
+	{
+		public:
+			CameraContext() {}
+			virtual bool init (std::string mask = "", int count = 0) { return true;};
+			virtual bool start(std::string mask = "", int count = 0) { return true;};
+			virtual bool stop() { return true;};
+			virtual bool getSelsectorMask(std::string *mask, std::vector<std::string> devices) { return true;}
+	};
+
+	class CameraStream {
+
+	public:
+
+		int width;
+		int height;
+		int channels;
+		bool online = false;
+
+		timeStamp_t timeStamp = 0;
+
+		// To implement
+		virtual bool init(int index, std::string file ) = 0;
+		virtual bool initRecorder( std::string file, std::string outputFormat ) {tkWRN("Recorder not provided");}
+		virtual bool closeRecorder() {  }
+		virtual bool close() = 0;
+
+		virtual bool readFrame( tk::data::ImageData &image ) = 0;
+
+		virtual bool writeFrame( tk::data::ImageData &image ) { return false; };
+
+	//protected:
+//
+    //    std::list<tk::data::ImageData*> rec_pool;
+	};
+
+}}
