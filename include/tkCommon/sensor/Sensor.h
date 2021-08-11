caa9c1a9e13a87235387b8da0881b422a0ea3ce8
Luca Bartoli <228618@studenti.unimore.it>
2021-08-11 16:37:29 +0200

    auv, sonar, sqpnp: added sqpnp library
    Signed-off-by: Luca Bartoli <228618@studenti.unimore.it>

--- include/tkCommon/sensor/Sensor.h
+++ include/tkCommon/sensor/Sensor.h
@@ -395,7 +395,7 @@ class Sensor {
 
             // error handling
             if (idx == -1) {
-                tkWRN("Timeout.\n");
+                //tkWRN("Timeout.\n");
             } else if (idx == -2) {
                 tkWRN("No free elemnt in the pool.\n");
             }
