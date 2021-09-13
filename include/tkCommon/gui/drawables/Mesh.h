40dc42521a60a86a4005dd1df6f3edd3f88dd73d
Fabio Bagni <fabio@pop-os.localdomain>
2021-09-14 00:21:15 +0200

    Draw mesh buffer

--- include/tkCommon/gui/drawables/Mesh.h
+++ include/tkCommon/gui/drawables/Mesh.h
@@ -6,7 +6,7 @@ namespace tk{ namespace gui{
 
 	class Mesh : public Drawable {
 
-        private:
+        protected:
             std::vector<tk::gui::Buffer<float>> obj;
             std::vector<tk::gui::Color_t> objColors;
             std::string filename;
@@ -24,6 +24,13 @@ namespace tk{ namespace gui{
             void imGuiInfos();
             void beforeDraw();
             void onClose();
+
+            std::vector<tk::gui::Buffer<float>> & getBuffer(){return obj;}
+            std::vector<tk::gui::Color_t> & getColors(){return objColors;}
+            std::vector<tk::gui::Color_t> & setColors(){return objColors;}
+            float getAmbientStrengt(){return ambientStrength;}
+            void setAmbientStrengt(float strength){ambientStrength = strength;}
+            void setLight(bool val){useLight = val;};
 
             std::string toString();
 	};
