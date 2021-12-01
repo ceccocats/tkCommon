78b8a606b2f54e5cab260ea3abae7f6e81f694a5
Francesco Gatti <gattifrancesco@hotmail.it>
2021-12-01 15:47:23 +0100

    tracking ok

--- include/tkCommon/gui/utils/deprecated_primitives.h
+++ include/tkCommon/gui/utils/deprecated_primitives.h
@@ -7,6 +7,7 @@ void drawAxis(float s, float lw = 2.5);
 void drawTexture(uint32_t tex, float sx, float sy);
 void genTexture(uint8_t *data, int width, int height, GLuint &tex);
 void drawCircle(tk::common::Vector3<float> pose, float r, int res=32, bool filled=false);
+void drawEllipse(tk::common::Vector3<float> pose, float rx, float ry, int res=32, bool filled=false);
 void drawLine(tk::common::Vector3<float> a, tk::common::Vector3<float> b, float w = 1);
 void drawCube(tk::common::Vector3<float> pose, tk::common::Vector3<float> size = {1,1,1}, bool filled=false);
 void drawRectangle(tk::common::Vector3<float> pose, tk::common::Vector2<float> size = {1,1}, bool filled=false);
